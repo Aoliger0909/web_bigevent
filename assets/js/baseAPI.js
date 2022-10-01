6529058ed21b2f7ef0b1b2ac1e0d7c0cfc5e6afa
xiaoAo <1298804439@qq.com>
2022-10-01 23:38:42 +0800

    完成了登录和注册的功能开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,11 @@
+//注意：每次调用$.get()/$.post()或$.ajax()时，会先调用ajaxPrefilter这个函数
+//在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function (options) {
+    console.log(options);   //{url: '/api/login', type: 'POST', isLocal: false, global: true, processData: true, …}
+    console.log(options.url);  //   /api/login
+
+    //在发起真正的Ajax请求之前，统一拼接请求的根路径
+    options.url = "http://www.liulongbin.top:3007" + options.url
+    console.log(options.url);  //http://www.liulongbin.top:3007/api/login
+
+})
